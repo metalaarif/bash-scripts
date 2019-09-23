aaebb5fb2f3465e42e6bb3fdbcf6a29d10266eae
metalaarif <metalaarif@gmail.com>
2019-09-23 16:23:57 +0100

    23sep2019

--- install-countdown.sh
+++ install-countdown.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-lsb_release -a
-apt update
+
+# This is compatible with only Ubuntu
+
+root_id=0
+release=`lsb_release -d | awk '{print $2, $3}'`
+update=`apt-get install git -y`
+#make_dir="mkdir /usr/bin/local/countdown"
+path="cd /tmp"
+clone_git="git clone https://github.com/metalaarif/CountdownScript.git"
+
+if [ $root_id != $UID ]; then
+	echo "You need to be root, you can use 'sudo -i' or 'su root' to become root."
+	exit
+fi
+
+#echo "OS Running:" $release
+#echo "Installing git ....."
+#echo $update
+
+$path
+$clone_git
+
 
