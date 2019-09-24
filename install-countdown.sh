138b3398089f24154e15969b44c6700a53fd4490
metalaarif <metalaarif@gmail.com>
2019-09-24 16:45:54 +0100

    finished script

--- install-countdown.sh
+++ install-countdown.sh
@@ -2,23 +2,47 @@
 
 # This is compatible with only Ubuntu
 
+## Making sure it needs to be run as root
 root_id=0
-release=`lsb_release -d | awk '{print $2, $3}'`
-update=`apt-get install git -y`
-#make_dir="mkdir /usr/bin/local/countdown"
-path="cd /tmp"
-clone_git="git clone https://github.com/metalaarif/CountdownScript.git"
-
 if [ $root_id != $UID ]; then
-	echo "You need to be root, you can use 'sudo -i' or 'su root' to become root."
-	exit
+        echo "You need to be root, Try 'sudo ./install_countdown'."
+        exit
 fi
 
-#echo "OS Running:" $release
-#echo "Installing git ....."
-#echo $update
+## Checking the OS
+release=`lsb_release -d | awk '{print $2, $3}'`
+echo "OS Running:" $release
 
-$path
-$clone_git
+## Checking if git is installed if not it will install it. 
+
+check_install=`dpkg --list git`
+install_git=`apt-get install git -y`
+echo $check_install > /dev/null
+
+if [ $?  == 0 ]; then
+	echo "Great! git is installed"
+else
+	echo "Installing Git...." $install_git
+fi
+
+## Creating Directory in /usr/local/bin and clone via git
+path_to_bin="cd /usr/local/bin"
+cloning="git clone https://github.com/metalaarif/CountdownScript.git"
+$path_to_bin
+$cloning
+
+## Deleting and changing the name
+`rm -rf CountdownScript/datetime.py `
+`rm -rf CountdownScript/countdown.py`
+`rm -rf CountdownScript/README.txt`
+`mv CountdownScript/countdown.sh CountdownScript/countdown`
+`chmod 777 CountdownScript/countdown`
+
+## Writing in Enviroment Variables
+
+`touch /etc/profile.d/countdown.sh`
+set_profile="/etc/profile.d/countdown.sh"
+echo "#!/bin/bash" > $set_profile
+echo "export PATH=/usr/local/bin/CountdownScript:$PATH" >> $set_profile
 
 
